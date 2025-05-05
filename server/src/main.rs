9d16204deadff6d3532b769408e3ce30f2142947
Elvin-19 <elvinmaubert@gmail.com>
2025-05-05 15:55:14 +0200

    git push

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,32 +1,44 @@
 use common::network;
-use std::net::{TcpListener, TcpStream};
-use std::thread;
-use std::io::{Read, Write};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::{
+        tcp::{OwnedReadHalf, OwnedWriteHalf},
+        TcpListener, TcpStream,
+    },
+    sync::Mutex,
+    spawn,
+};
 
-fn handle_client(mut stream: TcpStream) {
+
+async fn handle_client(mut stream: TcpStream) -> std::io::Result<()> {
     println!("Nouveau client connecté : {:?}", stream.peer_addr());
     let mut buffer = [0; 512];
-    match stream.read(&mut buffer) {
+    match stream.read(&mut buffer).await {
         Ok(size) => {
             println!("Données reçues : {}", String::from_utf8_lossy(&buffer[..size]));
-            let _ = stream.write_all(b"Commande re\xE7ue !");
+            let _ = stream.write_all(b"Commande re\xE7ue !").await;
         }
         Err(e) => println!("Erreur en lisant depuis le client : {}", e),
     }
+    Ok(())
 }
 
 /// TODO : Serde pour bdd json
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").expect("Impossible de lier le serveur sur le port 7878");
+
+#[tokio::main]
+async fn main() -> std::io::Result<()>{
+    let listener = TcpListener::bind("127.0.0.1:7878").await?;
     println!("Le serveur est en écoute sur le port 7878");
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                thread::spawn(move || handle_client(stream));
+    loop {
+        let (stream, addr) = listener.accept().await?;
+        println!("Nouveau client connecté : {:?}", addr);
+        spawn(async move {
+            if let Err(e) = handle_client(stream).await {
+                eprintln!("Error handling client: {:?}", e);
             }
-            Err(e) => println!("Erreur de connexion : {}", e),
-        }
+        });
+
     }
 }
